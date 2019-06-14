4f872a8465c2da9153b75ba81383c7023a34b2d7
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 18:22:55 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,3 +13,12 @@ let person = {
  
 person.greet();
 
+let sally = { name: 'Sally' };
+ 
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+ 
+greet.call(sally, 'Terry', 'George');
+
+
