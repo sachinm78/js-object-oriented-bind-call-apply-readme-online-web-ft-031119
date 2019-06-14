c32abcded8b8489e1bfd2a45fbd3841c158a3f13
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 18:23:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,24 +1,26 @@
-// use this to copy code snippets or use your browser's console
-
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
  
-greet(); // my name is , hi!
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
  
-let person = {
-    name: 'Bob',
-    greet: greet
-};
- 
-person.greet();
-
-let sally = { name: 'Sally' };
- 
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+    matchInterests(event) {
+        console.log("'this' is defined: ", this);
+        return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
+            return this.interests.includes(word);
+        });
+    }
 }
  
-greet.call(sally, 'Terry', 'George');
-
-
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
