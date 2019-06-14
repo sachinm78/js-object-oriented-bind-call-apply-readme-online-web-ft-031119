7838135c4b0091d213025bef5c62b750a047117f
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 18:22:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // use this to copy code snippets or use your browser's console
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+greet(); // my name is , hi!
+ 
+let person = {
+    name: 'Bob',
+    greet: greet
+};
+ 
+person.greet();
+
