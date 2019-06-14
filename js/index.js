05d9cf35869e732133abb07cacf4b8923e9c0258
Sachin Mahajan <sachinm78@gmail.com>
2019-06-14 18:23:16 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-// use this to copy code snippets or use your browser's console
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        console.log("'this' is defined: ", this);
+        return event.keywords.some(function(word) {
+            console.log("'this' is now undefined: ", this);
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
